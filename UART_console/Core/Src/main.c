3f21d30146fea814e40aceb0f69cf0c490728d3b
AntoninPutala <246922@vutbr.cz>
2026-03-11 09:43:48 +0100

    UART console
    autoread is working
    after each 1sec send
    data from selected sensors.

--- UART_console/Core/Src/main.c
+++ UART_console/Core/Src/main.c
@@ -62,6 +62,8 @@ static void MX_USART2_UART_Init(void);
 
 /* USER CODE BEGIN PFP */
 void setting_init(void);
+
+void autoread_fcn(uint32_t);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -79,6 +81,35 @@ void setting_init(void)
 	set_data.angle  = 90; // 90 deg
 	set_data.time_a = 10; // 10 * 0.1 = 1 sec
 	set_data.time_v = 20; // 20us
+}
+
+void autoread_fcn(uint32_t reg)
+{
+	if (_read_BV(reg, AUTOREAD_BIT))
+	{
+		// Previous event time
+		static uint32_t ticks = 0;
+
+		if (HAL_GetTick() > (ticks + set_data.time_a * 100))
+		{
+			ticks = HAL_GetTick();
+
+			if (_read_BV(reg, AUTOREAD_ACC_BIT))
+			{
+		    	printf("A: X=%d, Y=%d, Z=%d\n", measured_data.acc[0], measured_data.acc[1], measured_data.acc[2]);
+			}
+
+			if (_read_BV(reg, AUTOREAD_GYR_BIT))
+			{
+				printf("G: X=%d, Y=%d, Z=%d\n", measured_data.gyr[0], measured_data.gyr[1], measured_data.gyr[2]);
+			}
+
+			if (_read_BV(reg, AUTOREAD_MAG_BIT))
+			{
+				printf("M: X=%d, Y=%d, Z=%d\n", measured_data.mag[0], measured_data.mag[1], measured_data.mag[2]);
+			}
+		}
+	}
 }
 /* USER CODE END 0 */
 
@@ -149,10 +180,12 @@ int main(void)
 			uart_byte_available(b, &cmd_reg);
 
 			/* Demo - check cmd register */
-			printf("cmd_reg = %lx\n", cmd_reg);
+			// printf("cmd_reg = %lx\n", cmd_reg);
 		}
 
-		cmd_reg = 0;
+		// cmd_reg = 0;
+
+		autoread_fcn(cmd_reg);
 
 		/* USER CODE END WHILE */
 
